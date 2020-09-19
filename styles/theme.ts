bf590d6149a8bccb489662ce5204ed37a5f9104f
Brenda Profiro <brendaprofiro@gmail.com>
2020-09-19 15:11:51 -0300

    updated: configurar o design system

--- styles/theme.ts
+++ styles/theme.ts
@@ -2,6 +2,40 @@ import { theme, DefaultTheme } from '@chakra-ui/core';
 
 const customTheme: DefaultTheme = {
   ...theme,
+  fonts: {
+    body: 'Roboto, system-ui, snans-serif',
+    heading: 'Roboto, system-ui, snans-serif',
+    mono: 'Menlo, monospace'
+  },
+  fontWeights: {
+    ...theme.fontWeights,
+    normal: 400,
+    medium: 600,
+    bold: 700,
+  },
+  radii: {
+    ...theme.radii,
+    sm: '5px',
+    md: '8px',
+  },
+  fontSizes: {
+    ...theme.fontSizes,
+    '6xl': '54px',
+  },
+  colors: {
+    ...theme.colors,
+    blue: {
+      ...theme.colors.blue,
+      500: '#0041C1'
+    },
+    gray: {
+      ...theme.colors.gray,
+      300: '#E2E7ED',
+      600: '#6D7989',
+      700: '#414A5B',
+      800: '#1B2733'
+    }
+  }
 }
 
 export default customTheme;
